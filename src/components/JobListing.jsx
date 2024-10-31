9392921e88a7a96d9a78059d2f9e2981b6de3ba2
Diego Gomez <diego.gomez@email.com>
2024-10-31 17:07:33 -0500

    add react icons and importa map mark icon

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FaMapMarker } from 'react-icons/fa';
 
 const JobListing = ({job}) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
@@ -31,6 +32,7 @@ const JobListing = ({job}) => {
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
+                        <FaMapMarker className="inline text-lg ml-1 mr-1" />
                         <i className="fa-solid fa-location-dot text-lg"></i>
                         {job.location}
                     </div>
