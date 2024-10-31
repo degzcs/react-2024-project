e5b44b45e1341136c782364c6e3f68c78f475bb4
Diego Gomez <diego.gomez@email.com>
2024-10-31 17:02:09 -0500

    use state to show full desciption or less

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -1,6 +1,7 @@
-const ViewAllJobs = () => {
+        const ViewAllJobs = () => {
     return (
-        <section className="m-auto max-w-lg my-10 px-6">
+        <section className="m-
+            auto max-w-lg my-10 px-6">
             <a
                 href="jobs.html"
                 className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
