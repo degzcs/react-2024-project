ebb99116fd4d0fcaae03e1417ec7863aefaceaca
Diego Gomez <diego.gomez@email.com>
2024-11-01 09:31:54 -0500

    Add spinner component using react-spinners

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+    display: 'block',
+    margin: '100px auto',
+};
+
+const Spinner = ({loading}) => {
+    return (
+        <ClipLoader
+            loading={loading}
+            cssOverride={override}
+            color="#000"
+            size={150}
+        />
+    )
+}
+
+export default Spinner;
