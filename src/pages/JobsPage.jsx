9fdd004b12213f4a5be08be32dd0fd7dc625e0a2
Diego Gomez <diego.gomez@email.com>
2024-11-01 08:58:04 -0500

    add IsHome to show partial jobs

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,5 +1,9 @@
+import JobListings from '../components/JobListings';
+
 const JobsPage = () => {
-    return <div>Jobs Page</div>;
+    return <section className="bg-blue-50 px-4 py-6">
+        <JobListings />
+    </section>
 }
 
 export default JobsPage;
