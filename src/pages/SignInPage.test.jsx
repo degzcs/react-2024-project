2a93f0125a64fcf9cab7218f5edc7b05f5e5f0a9
Diego Gomez <diego.gomez@email.com>
2024-12-16 08:43:29 -0500

    create base tests plan

--- src/pages/SignInPage.test.jsx
+++ src/pages/SignInPage.test.jsx
@@ -4,4 +4,19 @@ describe ('SignInPage', () => {
     it('renders sign in page', () => {
         expect(true).toBe(true);
     });
+    it('validates the form', () => {
+        expect(true).toBe(true);
+    });
+    it('show a toast after successed login', () => {
+        expect(true).toBe(true);
+    });
+    it('show a toast after failed login', () => {
+        expect(true).toBe(true);
+    });
+    it('submits the form', () => {
+        expect(true).toBe(true);
+    });
+    it('redirects to home page', () => {
+        expect(true).toBe(true);
+    });
 })
